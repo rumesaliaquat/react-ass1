8b273f44c4a4178d29726aff22b71f3800018fdd
rumesaliaquat <rumesaliaquat@gmail.com>
2022-04-19 13:55:35 +0500

    3 commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inp, setInp] = useState('')
+  const [items, setItems] = useState([])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header"> */}
+        <input type='text' placeholder='Make a list for todo...' value={inp} onChange={(e) => {
+          setInp(e.target.value)
+        }} ></input>
+
+
+        <button onClick={() => {
+          setItems((oldItems) => {
+            return [...oldItems, inp]
+          })
+          setInp("")
+        }}>ADD ITEMS</button>
+        <button>DELETE ALL</button>
+
+        <ul>
+          {items.map((itemsVal, index) => {
+            return (
+            <>
+            <div>
+            <li>{itemsVal}</li>
+            <button onClick={() => {}}>EDIT</button>
+            <button onClick={() => {}}>DELETE</button>
+            </div> 
+            </> 
+            ) 
+          })}
+        </ul>
+      {/* </header> */}
     </div>
+
   );
 }
 
